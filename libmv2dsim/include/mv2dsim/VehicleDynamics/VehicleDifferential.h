0eb88a914f1ea5fc0406f8aa1ed11fa5eb43931f
Jose-Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2014-08-12 20:16:22 +0200

    First virtual classes

---
+++ libmv2dsim/include/mv2dsim/VehicleDynamics/VehicleDifferential.h
@@ -0,0 +1,16 @@
+/*+-------------------------------------------------------------------------+
+  |                       MultiVehicle 2D simulator (libmv2dsim)            |
+  |                                                                         |
+  | Copyright (C) 2014  Jose Luis Blanco Claraco (University of Almeria)    |
+  | Distributed under GNU General Public License version 3                  |
+  |   See <http://www.gnu.org/licenses/>                                    |
+  +-------------------------------------------------------------------------+  */
+
+#pragma once
+
+namespace mv2dsim
+{
+
+
+
+}
---
+++ libmv2dsim/src/VehicleDynamics/VehicleDifferential.cpp
@@ -0,0 +1,19 @@
+/*+-------------------------------------------------------------------------+
+  |                       MultiVehicle 2D simulator (libmv2dsim)            |
+  |                                                                         |
+  | Copyright (C) 2014  Jose Luis Blanco Claraco (University of Almeria)    |
+  | Distributed under GNU General Public License version 3                  |
+  |   See <http://www.gnu.org/licenses/>                                    |
+  +-------------------------------------------------------------------------+  */
+
+#include <cstdarg>
+#include <cstdlib>
+#include <cstdarg>
+#include <cstring>
+#include <float.h>
+#include <iostream>
+#include <algorithm>
+#include <cctype>
+#include <ctime>
+#include <cstdio>
+
