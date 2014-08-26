9a7af3750fd1b898926be0548074bcfea0ebd2c5
Jose-Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2014-08-26 22:17:39 +0200

    Ackermann sort of working!
    Former-commit-id: e7bcba8f0abe7f6e402851afa4317a198f903967

---
+++ libmv2dsim/src/VehicleDynamics/VehicleAckermann_ControllerRaw.cpp
@@ -0,0 +1,36 @@
+/*+-------------------------------------------------------------------------+
+  |                       MultiVehicle 2D simulator (libmv2dsim)            |
+  |                                                                         |
+  | Copyright (C) 2014  Jose Luis Blanco Claraco (University of Almeria)    |
+  | Distributed under GNU General Public License version 3                  |
+  |   See <http://www.gnu.org/licenses/>                                    |
+  +-------------------------------------------------------------------------+  */
+
+#include <mv2dsim/VehicleDynamics/VehicleAckermann.h>
+#include "xml_utils.h"
+
+using namespace mv2dsim;
+using namespace std;
+
+// See base class docs
+void DynamicsAckermann::ControllerRawForces::control_step(
+	const DynamicsAckermann::TControllerInput &ci, 
+	DynamicsAckermann::TControllerOutput &co)
+{
+	co.fl_torque = this->setpoint_wheel_torque_l;
+	co.fr_torque = this->setpoint_wheel_torque_r;
+	co.steer_ang = this->setpoint_steer_ang;
+}
+
+void DynamicsAckermann::ControllerRawForces::load_config(const rapidxml::xml_node<char>&node )
+{
+	std::map<std::string,TParamEntry> params;
+	params["fl_torque"] = TParamEntry("%lf", &setpoint_wheel_torque_l);
+	params["fr_torque"] = TParamEntry("%lf", &setpoint_wheel_torque_r);
+	
+	// Initial speed.
+	params["steer_ang_deg"] = TParamEntry("%lf_deg", &this->setpoint_steer_ang);
+
+	parse_xmlnode_children_as_param(node,params);
+}
+
