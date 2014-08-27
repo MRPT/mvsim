bc393283e40c9187a7c9c16acf15c505ac7a06eb
Jose Luis Blanco <joseluisblancoc@gmail.com>
2014-08-27 17:43:09 +0200

    Start of ROS interface
    Former-commit-id: 15ce21c2d88b811fbfe1d996da128a08a2a97538

---
+++ mv2dsim_node_src/mv2dsim_node.cpp
@@ -0,0 +1,27 @@
+#include <ros/ros.h>
+
+//#include <std_msgs/String.h>
+
+// Conversions ROS msgs <-> MRPT structs
+//#include <mrpt_bridge/mrpt_bridge.h>
+
+// MRPT stuff:
+#include <mrpt/version.h>
+#if MRPT_VERSION < 0x100
+#	error "This program requires MRPT >= 1.0.0"
+#endif
+
+
+using namespace std;
+
+
+/** The main */
+int main(int argc, char **argv)
+{
+	ros::init(argc, argv, "mv2dsim_node");
+
+
+	return 0;
+}
+
+
