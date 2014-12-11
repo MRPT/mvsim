5088322f5dadb57fdffb10bc2617715f0e4f87de
Jose Luis Blanco <joseluisblancoc@gmail.com>
2014-12-11 01:49:28 +0100

    First rudimentary ROS node.
    Former-commit-id: 475fd09820a339391f0a4d2d03c3f3aa036601d0

--- mvsim_node_src/mvsim_node.cpp
+++ mvsim_node_src/mvsim_node.cpp
@@ -1,27 +1,67 @@
-#include <ros/ros.h>
+/**
+  */
 
-//#include <std_msgs/String.h>
+#include "mvsim/mvsim_node_core.h"
 
-// Conversions ROS msgs <-> MRPT structs
-//#include <mrpt_bridge/mrpt_bridge.h>
+/*------------------------------------------------------------------------------
+ * MVSimNode()
+ * Constructor.
+ *----------------------------------------------------------------------------*/
 
-// MRPT stuff:
-#include <mrpt/version.h>
-#if MRPT_VERSION < 0x100
-#	error "This program requires MRPT >= 1.0.0"
-#endif
-
-
-using namespace std;
-
-
-/** The main */
-int main(int argc, char **argv)
+MVSimNode::MVSimNode()
 {
-	ros::init(argc, argv, "mvsim_node");
-
-
-	return 0;
 }
 
+/*------------------------------------------------------------------------------
+ * ~MVSimNode()
+ * Destructor.
+ *----------------------------------------------------------------------------*/
+
+MVSimNode::~MVSimNode()
+{
+}
+
+/*------------------------------------------------------------------------------
+ * publishMessage()
+ * Publish the message.
+ *----------------------------------------------------------------------------*/
+
+void MVSimNode::publishMessage(ros::Publisher *pub_message)
+{
+/*  mvsim::NodeExampleData msg;
+  msg.message = message;
+  msg.a = a;
+  msg.b = b;
+
+  pub_message->publish(msg);*/
+}
+
+/*------------------------------------------------------------------------------
+ * messageCallback()
+ * Callback function for subscriber.
+ *----------------------------------------------------------------------------*/
+
+/*void MVSimNode::messageCallback(const node_example::NodeExampleData::ConstPtr &msg)
+{
+  message = msg->message;
+  a = msg->a;
+  b = msg->b;
+
+  // Note that these are only set to INFO so they will print to a terminal for example purposes.
+  // Typically, they should be DEBUG.
+  ROS_INFO("message is %s", message.c_str());
+  ROS_INFO("sum of a + b = %d", a + b);
+}*/
+
+/*------------------------------------------------------------------------------
+ * configCallback()
+ * Callback function for dynamic reconfigure server.
+ *----------------------------------------------------------------------------*/
+
+void MVSimNode::configCallback(mvsim::mvsimNodeConfig &config, uint32_t level)
+{
+	// Set class variables to new values. They should match what is input at the dynamic reconfigure GUI.
+	//  message = config.message.c_str();
+	std::cout << "Debug: " << config.debug << std::endl;
+}
 
