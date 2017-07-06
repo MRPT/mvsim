ea565c49da31046f3c4a99e6b11be1b7f42ce835
Borys Tymchenko <tim4bor@gmail.com>
2017-07-07 00:00:36 +0300

    Added Ward-Iagnemma rolling resistance in debug mode
    Former-commit-id: 613852d371cfd79d72aec8856cb989fd236337f1

---
+++ libmvsim/include/mvsim/FrictionModels/WardIagnemmaFriction.h
@@ -0,0 +1,36 @@
+/*+-------------------------------------------------------------------------+
+  |                       MultiVehicle simulator (libmvsim)                 |
+  |                                                                         |
+  | Copyright (C) 2017  Borys Tymchenko                                     |
+  | Odessa National Polytechnic University                                  |
+  | Distributed under GNU General Public License version 3                  |
+  |   See <http://www.gnu.org/licenses/>                                    |
+  +-------------------------------------------------------------------------+  */
+
+#pragma once
+
+#include <mvsim/FrictionModels/FrictionBase.h>
+#include <vector>
+#include <Box2D/Dynamics/Joints/b2FrictionJoint.h>
+
+namespace mvsim
+{
+	/** Friction model implemented with respect to http://web.mit.edu/mobility/publications/Iagnemma_TRO_07.pdf
+	 * A Dynamic-Model-Based Wheel Slip Detector for Mobile Robots on Outdoor Terrain
+	 * Chris C. Ward and Karl Iagnemma
+	 */
+	class WardIagnemmaFriction : public FrictionBase
+	{
+		DECLARES_REGISTER_FRICTION(WardIagnemmaFriction)
+	public:
+		WardIagnemmaFriction(VehicleBase &my_vehicle, const rapidxml::xml_node<char> *node);
+
+		// See docs in base class.
+    virtual void evaluate_friction(const FrictionBase::TFrictionInput &input, mrpt::math::TPoint2D &out_result_force_local) const;
+
+	private:
+		double m_mu;  //!< friction coeficient (non-dimensional)
+		double m_C_damping; //!< For wheels "internal friction" (N*m*s/rad)
+    double m_A_roll, m_R1, m_R2; //!< Ward-Iagnemma rolling resistance coefficient
+	};
+}
