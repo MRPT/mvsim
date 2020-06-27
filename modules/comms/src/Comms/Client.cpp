2b2bfc87e2192b899054cbbc7d6fd77d86d6a2b0
Jose Luis Blanco Claraco <joseluisblancoc@gmail.com>
2020-06-27 11:20:27 +0200

    refactor Client without parallel thread

--- modules/comms/src/Comms/Client.cpp
+++ modules/comms/src/Comms/Client.cpp
@@ -31,7 +31,20 @@
 
 using namespace mvsim;
 
-Client::Client() : mrpt::system::COutputLogger("mvsim::Client") {}
+struct Client::ZMQImpl
+{
+#if defined(MVSIM_HAS_ZMQ)
+	zmq::context_t context{1};
+	zmq::socket_t mainReqSocket;
+#endif
+};
+
+Client::Client()
+	: mrpt::system::COutputLogger("mvsim::Client"),
+	  m_zmq(mrpt::make_impl<std::shared_ptr<Client::ZMQImpl>>(
+		  std::make_shared<Client::ZMQImpl>()))
+{
+}
 Client::Client(const std::string& nodeName) : Client() { setName(nodeName); }
 
 Client::~Client() { shutdown(); }
@@ -40,14 +53,19 @@ void Client::setName(const std::string& nodeName) { nodeName_ = nodeName; }
 
 void Client::connect()
 {
-	ASSERTMSG_(!mainThread_.joinable(), "Client is already running.");
+	using namespace std::string_literals;
+	ASSERTMSG_(
+		!(*m_zmq)->mainReqSocket.connected(), "Client is already running.");
 
 #if defined(MVSIM_HAS_ZMQ) && defined(MVSIM_HAS_PROTOBUF)
-	mainThread_ = std::thread(&Client::internalClientThread, this);
 
-#if MRPT_VERSION >= 0x204
-	mrpt::system::thread_name("clientMain", mainThread_);
-#endif
+	(*m_zmq)->mainReqSocket = zmq::socket_t((*m_zmq)->context, ZMQ_REQ);
+	(*m_zmq)->mainReqSocket.connect(
+		"tcp://"s + serverHostAddress_ + ":"s +
+		std::to_string(MVSIM_PORTNO_MAIN_REP));
+
+	// Let the server know about this new node:
+	doRegisterClient((*m_zmq)->mainReqSocket);
 
 #else
 	THROW_EXCEPTION(
@@ -58,12 +76,14 @@ void Client::connect()
 
 void Client::shutdown() noexcept
 {
+#if defined(MVSIM_HAS_ZMQ) && defined(MVSIM_HAS_PROTOBUF)
+
+	if (!(*m_zmq)->mainReqSocket.connected()) return;
+
 	try
 	{
-		MRPT_LOG_DEBUG_STREAM("Waiting for the thread to quit.");
-		requestMainThreadTermination();
-
-		if (mainThread_.joinable()) mainThread_.join();
+		MRPT_LOG_DEBUG_STREAM("Unregistering from server.");
+		doUnregisterClient((*m_zmq)->mainReqSocket);
 
 		MRPT_LOG_DEBUG_STREAM("Joined thread.");
 	}
@@ -72,82 +92,14 @@ void Client::shutdown() noexcept
 		MRPT_LOG_ERROR_STREAM(
 			"shutdown: Exception: " << mrpt::exception_to_str(e));
 	}
-}
 
-void Client::internalClientThread()
-{
-	using namespace std::string_literals;
-
-#if defined(MVSIM_HAS_ZMQ) && defined(MVSIM_HAS_PROTOBUF)
-	try
-	{
-		MRPT_LOG_INFO_STREAM("Client thread started.");
-
-		zmq::context_t context(1);
-		mainThreadZMQcontext_ = &context;
-
-		zmq::socket_t mainReqSocket(context, ZMQ_REQ);
-		mainReqSocket.connect(
-			"tcp://"s + serverHostAddress_ + ":"s +
-			std::to_string(MVSIM_PORTNO_MAIN_REP));
-
-		// Let the server know about this new node:
-		doRegisterClient(mainReqSocket);
-
-		while (!mainThreadMustExit_)
-		{
-			std::this_thread::sleep_for(std::chrono::milliseconds(100));
-		}
-
-		MRPT_LOG_DEBUG_STREAM("Unregistering from server.");
-		doUnregisterClient(mainReqSocket);
-	}
-	catch (const zmq::error_t& e)
-	{
-		if (e.num() == ETERM)
-		{
-			// This simply means someone called
-			// requestMainThreadTermination(). Just exit silently.
-			MRPT_LOG_INFO_STREAM(
-				"Client thread about to exit for ZMQ term signal.");
-		}
-		else
-		{
-			MRPT_LOG_ERROR_STREAM(
-				"internalClientThread: ZMQ error: " << e.what());
-		}
-	}
-	catch (const std::exception& e)
-	{
-		MRPT_LOG_ERROR_STREAM(
-			"internalClientThread: Exception: " << mrpt::exception_to_str(e));
-	}
-
-	MRPT_LOG_INFO_STREAM("Client thread quitted.");
-
-	mainThreadZMQcontext_ = nullptr;
-#endif
-}
-
-void Client::requestMainThreadTermination()
-{
-#if defined(MVSIM_HAS_ZMQ) && defined(MVSIM_HAS_PROTOBUF)
-	MRPT_LOG_INFO("requestMainThreadTermination() called.");
-
-	mainThreadMustExit_ = true;
-	// Give time to unregister
-	std::this_thread::sleep_for(std::chrono::milliseconds(200));
-
-	zmq::context_t* ctx = mainThreadZMQcontext_;
-	if (ctx)
-	{
 #if ZMQ_VERSION >= ZMQ_MAKE_VERSION(4, 4, 0)
-		ctx->shutdown();
+	(*m_zmq)->context.shutdown();
 #else
-		// Missing shutdown() in older versions:
-		zmq_ctx_shutdown(ctx->operator void*());
+	// Missing shutdown() in older versions:
+	zmq_ctx_shutdown((*m_zmq)->context.operator void*());
 #endif
-	}
+
 #endif
 }
 
