91ffe0c03f74544223566d731ccd533da43e31ae
Jose Luis Blanco Claraco <joseluisblancoc@gmail.com>
2020-08-10 22:19:09 +0200

    walls progress

--- modules/simulator/src/World_walls.cpp
+++ modules/simulator/src/World_walls.cpp
@@ -11,6 +11,7 @@
 #include <mrpt/opengl/CAssimpModel.h>
 #include <mrpt/system/filesystem.h>
 #include <mvsim/World.h>
+
 #include <rapidxml.hpp>
 #include <rapidxml_print.hpp>
 #include <rapidxml_utils.hpp>
@@ -28,11 +29,13 @@ void World::processLoadWalls(const rapidxml::xml_node<char>& node)
 	ASSERT_(0 == strcmp(node.name(), "walls"));
 
 	std::string wallModelFileName;
+	std::string sTransformation;
 	double wallThickness = 0.10;
 	double scale = 1.0;
 
 	TParameterDefinitions params;
 	params["model_uri"] = TParamEntry("%s", &wallModelFileName);
+	params["transformation"] = TParamEntry("%s", &sTransformation);
 	params["wallThickness"] = TParamEntry("%lf", &wallThickness);
 	params["scale"] = TParamEntry("%lf", &scale);
 
@@ -43,6 +46,20 @@ void World::processLoadWalls(const rapidxml::xml_node<char>& node)
 	const std::string localFileName = xmlPathToActualPath(wallModelFileName);
 	ASSERT_FILE_EXISTS_(localFileName);
 
+	// Optional transformation:
+	auto HM = mrpt::math::CMatrixDouble44::Identity();
+	if (!sTransformation.empty())
+	{
+		std::stringstream ssError;
+		bool ok = HM.fromMatlabStringFormat(sTransformation, ssError);
+		if (!ok)
+			THROW_EXCEPTION_FMT(
+				"Error parsing 'transformation=\"%s\"' parameter of walls:\n%s",
+				sTransformation.c_str(), ssError.str().c_str());
+		MRPT_LOG_DEBUG_STREAM("Walls: using transformation: " << HM.asString());
+	}
+	const auto tf = mrpt::poses::CPose3D(HM);
+
 	MRPT_LOG_DEBUG_STREAM(
 		"Loading walls definition model from: " << localFileName);
 
@@ -52,10 +69,13 @@ void World::processLoadWalls(const rapidxml::xml_node<char>& node)
 	const auto& points = glModel->shaderWireframeVertexPointBuffer();
 	MRPT_LOG_DEBUG_STREAM("Walls loaded, " << points.size() << " segments.");
 
-	for (const auto& pt : points)
-	{
-		std::cout << "pt: " << pt << "\n";
-	}
+	// Transform them:
+	std::vector<mrpt::math::TPoint3Df> tfPts;
+	tfPts.reserve(points.size());
+	for (const auto& pt : points) tfPts.emplace_back(tf.composePoint(pt));
+
+	// Create walls themselves:
+	MRPT_TODO("Continue");
 
 	MRPT_END
 }
