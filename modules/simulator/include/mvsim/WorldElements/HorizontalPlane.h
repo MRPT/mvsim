f39754eba25825720c41d1df1b80920a829750ad
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2021-12-27 18:31:53 +0100

    enable textures in planes

--- modules/simulator/include/mvsim/WorldElements/HorizontalPlane.h
+++ modules/simulator/include/mvsim/WorldElements/HorizontalPlane.h
@@ -10,6 +10,7 @@
 #pragma once
 
 #include <mrpt/img/TColor.h>
+#include <mrpt/opengl/CSetOfTexturedTriangles.h>
 #include <mrpt/opengl/CTexturedPlane.h>
 #include <mvsim/WorldElements/WorldElementBase.h>
 
@@ -31,11 +32,18 @@ class HorizontalPlane : public WorldElementBase
 		mrpt::opengl::COpenGLScene& viz, mrpt::opengl::COpenGLScene& physical,
 		bool childrenOnly) override;
 
-	double m_x_min = -10, m_x_max = 10, m_y_min = -10, m_y_max = 10;
+	float m_x_min = -10, m_x_max = 10, m_y_min = -10, m_y_max = 10;
 	mrpt::img::TColor m_color = {0xa0, 0xa0, 0xa0, 0xff};
-	double m_z = .0;
+
+	/** If defined, it overrides the plain color in m_color */
+	std::string m_textureFileName;
+	double m_textureSizeX = 1.0;
+	double m_textureSizeY = 1.0;
+
+	float m_z = .0f;
 	std::string m_cull_faces = "NONE";
 
 	mrpt::opengl::CTexturedPlane::Ptr m_gl_plane;
+	mrpt::opengl::CSetOfTexturedTriangles::Ptr m_gl_plane_text;
 };
 }  // namespace mvsim
