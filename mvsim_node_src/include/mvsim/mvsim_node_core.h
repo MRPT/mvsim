c959c289291c1150c022c2cc6a9a58c0d5623810
Jose Luis Blanco Claraco <joseluisblancoc@gmail.com>
2022-03-05 23:11:07 +0100

    Fix compilation of the ROS1 node against the latest mvsim libraries
    Former-commit-id: 069da5b0ae3eb0f1db0996fd3cafe2d7abe8c41a [formerly 953675a8e7ccb6b6436c7d432def26833cb51679]
    Former-commit-id: 09bb83a810e0dd8a53576f81cf9ca684588cada6

--- mvsim_node_src/include/mvsim/mvsim_node_core.h
+++ mvsim_node_src/include/mvsim/mvsim_node_core.h
@@ -4,31 +4,19 @@
 #ifndef SR_MVSIM_NODE_CORE_H
 #define SR_MVSIM_NODE_CORE_H
 
-// ROS includes.
+#include <dynamic_reconfigure/server.h>
+#include <mrpt/obs/CObservation.h>
+#include <mrpt/system/CTicTac.h>
+#include <mvsim/mvsim.h>  // the mvsim library
+#include <mvsim/mvsimNodeConfig.h>
 #include <ros/ros.h>
 #include <ros/time.h>
 #include <rosgraph_msgs/Clock.h>
-
-// Dynamic reconfigure includes.
-#include <dynamic_reconfigure/server.h>
 #include <tf/transform_broadcaster.h>
 #include <tf2_ros/static_transform_broadcaster.h>
 #include <visualization_msgs/MarkerArray.h>
 
 #include <thread>
-
-// Auto-generated from cfg/ directory.
-#include <mrpt/utils/CTicTac.h>
-#include <mvsim/mvsim.h>  // the mvsim library
-#include <mvsim/mvsimNodeConfig.h>
-
-#if MRPT_VERSION >= 0x130
-#include <mrpt/obs/CObservation.h>
-using mrpt::obs::CObservation;
-#else
-#include <mrpt/slam/CObservation.h>
-using mrpt::slam::CObservation;
-#endif
 
 /** A class to wrap libmvsim as a ROS node
  */
@@ -48,54 +36,57 @@ class MVSimNode
 	void configCallback(mvsim::mvsimNodeConfig& config, uint32_t level);
 
 	void onNewObservation(
-		const mvsim::VehicleBase& veh, const mrpt::obs::CObservation::Ptr& obs);
+		const mvsim::Simulable& veh, const mrpt::obs::CObservation::Ptr& obs);
 
 	/// The mvsim library simulated world (includes everything: vehicles,
 	/// obstacles, etc.)
 	mvsim::World mvsim_world_;
 
-	double realtime_factor_;  //!< (Defaul=1.0) >1: speed-up, <1: slow-down
-	int gui_refresh_period_ms_;	 //!< Default:25
-	bool m_show_gui;  //!< Default= true
-	bool m_do_fake_localization;  //!< Default=true. Behaves as
-								  //! navigation/fake_localization for each
-								  //! vehicle without the need to launch
-								  //! additional nodes.
-	double m_transform_tolerance;  //!< (Default=0.1) Time tolerance for
-								   //! published TFs
+	/// (Defaul=1.0) >1: speed-up, <1: slow-down
+	double realtime_factor_ = 1.0;
+	int gui_refresh_period_ms_ = 50;
+	bool show_gui_ = true;	//!< Default= true
+
+	/// Default=true. Behaves as navigation/fake_localization for each vehicle
+	/// without the need to launch additional nodes.
+	bool do_fake_localization_ = true;
+
+	//!< (Default=0.1) Time tolerance for published TFs
+	double m_transform_tolerance = 0.1;
 
    protected:
-	ros::NodeHandle& m_n;
-	ros::NodeHandle m_localn;
+	ros::NodeHandle& n_;
+	ros::NodeHandle localn_{"~"};
 
 	// === ROS Publishers ====
-	ros::Publisher m_pub_map_ros,
-		m_pub_map_metadata;	 //!< used for simul_map publication
-	ros::Publisher m_pub_clock;
-	tf::TransformBroadcaster m_tf_br;  //!< Use to send data to TF
-	tf2_ros::StaticTransformBroadcaster
-		m_static_tf_br;	 //!< For sending STATIC tf
+	/// used for simul_map publication
+	ros::Publisher pub_map_ros_, pub_map_metadata_;
+	ros::Publisher pub_clock_;
+	tf::TransformBroadcaster tf_br_;  //!< Use to send data to TF
+
+	/// For sending STATIC tf
+	tf2_ros::StaticTransformBroadcaster static_tf_br_;
 
 	struct TPubSubPerVehicle
 	{
-		ros::Subscriber
-			sub_cmd_vel;  //!< Subscribers for each vehicle's "cmd_vel" topic
+		ros::Subscriber sub_cmd_vel;  //!< Subscribers vehicle's "cmd_vel" topic
 		ros::Publisher pub_odom;  //!< Publisher of "odom" topic
-		ros::Publisher
-			pub_ground_truth;  //!< Publisher of "base_pose_ground_truth" topic
-		ros::Publisher pub_amcl_pose,
-			pub_particlecloud;	//!< Publishers for "fake_localization" topics
-		std::map<std::string, ros::Publisher>
-			pub_sensors;  //!< Map <sensor_label> => publisher
+		ros::Publisher pub_ground_truth;  //!< "base_pose_ground_truth" topic
+
+		/// "fake_localization" pubs:
+		ros::Publisher pub_amcl_pose, pub_particlecloud;
+
+		/// Map <sensor_label> => publisher
+		std::map<std::string, ros::Publisher> pub_sensors;
+
 		ros::Publisher pub_chassis_markers;	 //!< "<VEH>/chassis_markers"
 		ros::Publisher pub_chassis_shape;  //!< "<VEH>/chassis_shape"
 		visualization_msgs::MarkerArray chassis_shape_msg;
 	};
 
-	std::vector<TPubSubPerVehicle>
-		m_pubsub_vehicles;	//!< Pubs/Subs for each vehicle. Initialized by
-							//! initPubSubs(), called from
-							//! notifyROSWorldIsUpdated()
+	/// Pubs/Subs for each vehicle. Initialized by initPubSubs(), called from
+	/// notifyROSWorldIsUpdated()
+	std::vector<TPubSubPerVehicle> m_pubsub_vehicles;
 
 	/** Initialize all pub/subs required for each vehicle, for the specific
 	 * vehicle \a veh */
@@ -108,13 +99,11 @@ class MVSimNode
 		const geometry_msgs::Twist::ConstPtr& cmd, mvsim::VehicleBase* veh);
 	// === End ROS Hooks====
 
-	rosgraph_msgs::Clock m_clockMsg;
-	ros::Time m_sim_time;  //!< Current simulation time
-	ros::Time
-		m_base_last_cmd;  //!< Last time we received a vel_cmd (for watchdog)
-	ros::Duration m_base_watchdog_timeout;
-	const tf::Transform
-		m_tfIdentity;  //!< Unit transform (const, created only once)
+	rosgraph_msgs::Clock clockMsg_;
+	ros::Time sim_time_;  //!< Current simulation time
+	ros::Time base_last_cmd_;  //!< received a vel_cmd (for watchdog)
+	ros::Duration base_watchdog_timeout_;
+	const tf::Transform tfIdentity_;  //!< Unit transform (const, once)
 
 	struct TThreadParams
 	{
@@ -125,23 +114,26 @@ class MVSimNode
 	TThreadParams thread_params_;
 	mrpt::system::CTicTac realtime_tictac_;
 
-	double t_old_;	// = realtime_tictac_.Tac();
-	bool world_init_ok_;  //!< will be true after a success call to
-						  //! loadWorldModel()
+	double t_old_ = -1;
 
-	double m_period_ms_publish_tf;	//!< Minimum period between publication of
-									//! TF transforms & /*/odom topics (In ms)
-	mrpt::system::CTicTac m_tim_publish_tf;
+	/// will be true after a success call to loadWorldModel()
+	bool world_init_ok_ = false;
 
-	double m_period_ms_teleop_refresh;	//!< Minimum period between update of
-										//! live info & read of teleop key
-										//! strokes in GUI (In ms)
-	mrpt::system::CTicTac m_tim_teleop_refresh;
+	/// Minimum period between publication of TF transforms & /*/odom topics (In
+	/// ms)
+	double period_ms_publish_tf_ = 20;
 
-	size_t m_teleop_idx_veh;  //!< for teleoperation from the GUI (selects the
-							  //!"focused" vehicle)
-	mvsim::World::TGUIKeyEvent m_gui_key_events;
-	std::string m_msg2gui;
+	mrpt::system::CTicTac tim_publish_tf_;
+
+	/// Minimum period between update of live info & read of teleop key strokes
+	/// in GUI (In ms)
+	double period_ms_teleop_refresh_ = 100;
+	mrpt::system::CTicTac tim_teleop_refresh_;
+
+	/// for teleoperation from the GUI (selects the focused" vehicle)
+	size_t teleop_idx_veh_ = 0;
+	mvsim::World::TGUIKeyEvent gui_key_events_;
+	std::string msg2gui_;
 
 	std::thread thGUI_;
 	static void thread_update_GUI(TThreadParams& thread_params);
@@ -156,7 +148,7 @@ class MVSimNode
 	/** Creates the string "/<VEH_NAME>/<VAR_NAME>" if there're more than one
 	 * vehicle in the World, or "/<VAR_NAME>" otherwise. */
 	std::string vehVarName(
-		const std::string& sVarName, const mvsim::VehicleBase* veh) const;
+		const std::string& sVarName, const mvsim::VehicleBase& veh) const;
 
 	void sendStaticTF(
 		const std::string& frame_id, const std::string& child_frame_id,
@@ -170,10 +162,10 @@ class MVSimNode
 		void visit(mvsim::WorldElementBase* obj);
 
 		MVSimVisitor_notifyROSWorldIsUpdated(MVSimNode& parent)
-			: m_parent(parent)
+			: parent_(parent)
 		{
 		}
-		MVSimNode& m_parent;
+		MVSimNode& parent_;
 	};
 
 };	// end class
