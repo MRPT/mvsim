68eb40aec7e9ed8e296ceb646eb616f06898424f
Jose Luis Blanco Claraco <joseluisblancoc@gmail.com>
2022-07-03 11:35:21 +0200

    Add python wrapper for pybind 2.9
    Former-commit-id: 3d3bbc9195b4c40abca25cc09a5a537042681f69 [formerly 28c4a0b88b787a567cc11ce822d67b08d154b342]
    Former-commit-id: f95da739473dd129b7e7421cc52dbc16ea83e658

---
+++ modules/comms/python/generated-sources-pybind2.9/mvsim/Comms/Client.cpp
@@ -0,0 +1,60 @@
+#include <functional>
+#include <iterator>
+#include <memory>
+#include <mrpt/system/CTimeLogger.h>
+#include <mvsim/Comms/Client.h>
+#include <sstream> // __str__
+#include <string>
+#include <string_view>
+
+#include <functional>
+#include <pybind11/pybind11.h>
+#include <string>
+#include <pybind11/stl.h>
+
+
+#ifndef BINDER_PYBIND11_TYPE_CASTER
+	#define BINDER_PYBIND11_TYPE_CASTER
+	PYBIND11_DECLARE_HOLDER_TYPE(T, std::shared_ptr<T>)
+	PYBIND11_DECLARE_HOLDER_TYPE(T, T*)
+	PYBIND11_MAKE_OPAQUE(std::shared_ptr<void>)
+#endif
+
+void bind_mvsim_Comms_Client(std::function< pybind11::module &(std::string const &namespace_) > &M)
+{
+	{ // mvsim::Client file:mvsim/Comms/Client.h line:48
+		pybind11::class_<mvsim::Client, std::shared_ptr<mvsim::Client>> cl(M("mvsim"), "Client", "This is the connection of any user program with the MVSIM server, so\n it can advertise and subscribe to topics and use remote services.\n\n Users should instance a class mvsim::Client (C++) or mvsim.Client (Python) to\n communicate with the simulation runnin in mvsim::World or any other module.\n\n Usage:\n  - Instantiate a Client object.\n  - Call connect(). It will return immediately.\n  - The client will be working on the background as long as the object is not\n destroyed.\n\n Messages and topics are described as Protobuf messages, and communications\n are done via ZMQ sockets.\n\n See: https://mvsimulator.readthedocs.io/\n\n \n\n ");
+		cl.def( pybind11::init( [](){ return new mvsim::Client(); } ) );
+		cl.def( pybind11::init<const std::string &>(), pybind11::arg("nodeName") );
+
+		cl.def("setName", (void (mvsim::Client::*)(const std::string &)) &mvsim::Client::setName, "@{ \n\nC++: mvsim::Client::setName(const std::string &) --> void", pybind11::arg("nodeName"));
+		cl.def("serverHostAddress", (const std::string & (mvsim::Client::*)() const) &mvsim::Client::serverHostAddress, "C++: mvsim::Client::serverHostAddress() const --> const std::string &", pybind11::return_value_policy::automatic);
+		cl.def("serverHostAddress", (void (mvsim::Client::*)(const std::string &)) &mvsim::Client::serverHostAddress, "C++: mvsim::Client::serverHostAddress(const std::string &) --> void", pybind11::arg("serverIpOrAddressName"));
+		cl.def("connect", (void (mvsim::Client::*)()) &mvsim::Client::connect, "Connects to the server in a parallel thread.\n  Default server address is `localhost`, can be changed with\n serverHostAddress().\n\nC++: mvsim::Client::connect() --> void");
+		cl.def("connected", (bool (mvsim::Client::*)() const) &mvsim::Client::connected, "Whether the client is correctly connected to the server. \n\nC++: mvsim::Client::connected() const --> bool");
+		cl.def("shutdown", (void (mvsim::Client::*)()) &mvsim::Client::shutdown, "Shutdowns the communication thread. Blocks until the thread is stopped.\n There is no need to manually call this method, it is called upon\n destruction. \n\nC++: mvsim::Client::shutdown() --> void");
+		cl.def("callService", (std::string (mvsim::Client::*)(const std::string &, const std::string &)) &mvsim::Client::callService, "C++: mvsim::Client::callService(const std::string &, const std::string &) --> std::string", pybind11::arg("serviceName"), pybind11::arg("inputSerializedMsg"));
+		cl.def("subscribe_topic_raw", (void (mvsim::Client::*)(const std::string &, const class std::function<void (const class zmq::message_t &)> &)) &mvsim::Client::subscribe_topic_raw, "C++: mvsim::Client::subscribe_topic_raw(const std::string &, const class std::function<void (const class zmq::message_t &)> &) --> void", pybind11::arg("topicName"), pybind11::arg("callback"));
+		cl.def("enable_profiler", (void (mvsim::Client::*)(bool)) &mvsim::Client::enable_profiler, "C++: mvsim::Client::enable_profiler(bool) --> void", pybind11::arg("enable"));
+
+		{ // mvsim::Client::InfoPerNode file:mvsim/Comms/Client.h line:106
+			auto & enclosing_class = cl;
+			pybind11::class_<mvsim::Client::InfoPerNode, std::shared_ptr<mvsim::Client::InfoPerNode>> cl(enclosing_class, "InfoPerNode", "");
+			cl.def( pybind11::init( [](){ return new mvsim::Client::InfoPerNode(); } ) );
+			cl.def( pybind11::init( [](mvsim::Client::InfoPerNode const &o){ return new mvsim::Client::InfoPerNode(o); } ) );
+			cl.def_readwrite("name", &mvsim::Client::InfoPerNode::name);
+		}
+
+		{ // mvsim::Client::InfoPerTopic file:mvsim/Comms/Client.h line:112
+			auto & enclosing_class = cl;
+			pybind11::class_<mvsim::Client::InfoPerTopic, std::shared_ptr<mvsim::Client::InfoPerTopic>> cl(enclosing_class, "InfoPerTopic", "");
+			cl.def( pybind11::init( [](){ return new mvsim::Client::InfoPerTopic(); } ) );
+			cl.def( pybind11::init( [](mvsim::Client::InfoPerTopic const &o){ return new mvsim::Client::InfoPerTopic(o); } ) );
+			cl.def_readwrite("name", &mvsim::Client::InfoPerTopic::name);
+			cl.def_readwrite("type", &mvsim::Client::InfoPerTopic::type);
+			cl.def_readwrite("endpoints", &mvsim::Client::InfoPerTopic::endpoints);
+			cl.def_readwrite("publishers", &mvsim::Client::InfoPerTopic::publishers);
+		}
+
+	}
+}
---
+++ modules/comms/python/generated-sources-pybind2.9/std/stdexcept.cpp
@@ -0,0 +1,52 @@
+#include <iterator>
+#include <memory>
+#include <sstream> // __str__
+#include <stdexcept>
+#include <string>
+
+#include <functional>
+#include <pybind11/pybind11.h>
+#include <string>
+#include <pybind11/stl.h>
+
+
+#ifndef BINDER_PYBIND11_TYPE_CASTER
+	#define BINDER_PYBIND11_TYPE_CASTER
+	PYBIND11_DECLARE_HOLDER_TYPE(T, std::shared_ptr<T>)
+	PYBIND11_DECLARE_HOLDER_TYPE(T, T*)
+	PYBIND11_MAKE_OPAQUE(std::shared_ptr<void>)
+#endif
+
+// std::runtime_error file:stdexcept line:219
+struct PyCallBack_std_runtime_error : public std::runtime_error {
+	using std::runtime_error::runtime_error;
+
+	const char * what() const throw() override {
+		pybind11::gil_scoped_acquire gil;
+		pybind11::function overload = pybind11::get_overload(static_cast<const std::runtime_error *>(this), "what");
+		if (overload) {
+			auto o = overload.operator()<pybind11::return_value_policy::reference>();
+			if (pybind11::detail::cast_is_temporary_value_reference<const char *>::value) {
+				static pybind11::detail::override_caster_t<const char *> caster;
+				return pybind11::detail::cast_ref<const char *>(std::move(o), caster);
+			}
+			else return pybind11::detail::cast_safe<const char *>(std::move(o));
+		}
+		return runtime_error::what();
+	}
+};
+
+void bind_std_stdexcept(std::function< pybind11::module &(std::string const &namespace_) > &M)
+{
+	{ // std::runtime_error file:stdexcept line:219
+		pybind11::class_<std::runtime_error, std::shared_ptr<std::runtime_error>, PyCallBack_std_runtime_error, std::exception> cl(M("std"), "runtime_error", "");
+		cl.def( pybind11::init<const std::string &>(), pybind11::arg("__arg") );
+
+		cl.def( pybind11::init<const char *>(), pybind11::arg("") );
+
+		cl.def( pybind11::init( [](PyCallBack_std_runtime_error const &o){ return new PyCallBack_std_runtime_error(o); } ) );
+		cl.def( pybind11::init( [](std::runtime_error const &o){ return new std::runtime_error(o); } ) );
+		cl.def("assign", (class std::runtime_error & (std::runtime_error::*)(const class std::runtime_error &)) &std::runtime_error::operator=, "C++: std::runtime_error::operator=(const class std::runtime_error &) --> class std::runtime_error &", pybind11::return_value_policy::automatic, pybind11::arg(""));
+		cl.def("what", (const char * (std::runtime_error::*)() const) &std::runtime_error::what, "C++: std::runtime_error::what() const --> const char *", pybind11::return_value_policy::automatic);
+	}
+}
