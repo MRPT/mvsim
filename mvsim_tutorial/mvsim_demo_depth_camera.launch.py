0e67534828540a373f7b5b46230e8e6165c1d361
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2022-09-27 13:39:59 +0200

    add more ros2 demos

---
+++ mvsim_tutorial/mvsim_demo_depth_camera.launch.py
@@ -0,0 +1,47 @@
+
+# ROS2 launch file
+
+from launch import LaunchDescription
+from launch.substitutions import TextSubstitution
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from ament_index_python import get_package_share_directory
+import os
+
+
+def generate_launch_description():
+    mvsimDir = get_package_share_directory("mvsim")
+    #print('mvsimDir: ' + mvsimDir)
+
+    # args that can be set from the command line or a default will be used
+    world_file_launch_arg = DeclareLaunchArgument(
+        "world_file", default_value=TextSubstitution(
+            text=os.path.join(mvsimDir, 'mvsim_tutorial', 'mvsim_demo_depth_camera.world.xml')))
+
+    mvsim_node = Node(
+        package='mvsim',
+        executable='mvsim_node',
+        name='mvsim',
+        output='screen',
+        parameters=[
+            os.path.join(mvsimDir, 'mvsim_tutorial',
+                         'mvsim_demo_depth_camera.yaml'),
+            {
+                "world_file": LaunchConfiguration('world_file'),
+            }]
+    )
+
+    rviz2_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        arguments=[
+                '-d', [os.path.join(mvsimDir, 'mvsim_tutorial', 'mvsim_demo_depth_camera_ros2.rviz')]]
+    )
+
+    return LaunchDescription([
+        world_file_launch_arg,
+        mvsim_node,
+        rviz2_node
+    ])
