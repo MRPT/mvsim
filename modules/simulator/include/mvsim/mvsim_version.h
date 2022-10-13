b46fbf3facd3d536fd081ba0e62dab97e294ea96
Jose Luis Blanco Claraco <joseluisblancoc@gmail.com>
2022-10-13 23:00:54 +0200

    fix missing file in last commit
    Former-commit-id: fb7219a5c89db3198084e1f15877ec14c0d2fb55 [formerly 07f277b8e485e8c124b4270658c2f58c097c3d55]
    Former-commit-id: 4ee14d64dbdd2bac6c05c1af0fa92906abe896a2

---
+++ modules/simulator/include/mvsim/mvsim_version.h
@@ -0,0 +1,21 @@
+/*+-------------------------------------------------------------------------+
+  |                       MultiVehicle simulator (libmvsim)                 |
+  |                                                                         |
+  | Copyright (C) 2014-2022  Jose Luis Blanco Claraco                       |
+  | Copyright (C) 2017  Borys Tymchenko (Odessa Polytechnic University)     |
+  | Distributed under 3-clause BSD License                                  |
+  |   See COPYING                                                           |
+  +-------------------------------------------------------------------------+ */
+
+#pragma once
+
+// clang-format off
+#define MVSIM_MAJOR_VERSION 0
+#define MVSIM_MINOR_VERSION 4
+#define MVSIM_PATCH_VERSION 1
+
+#define MVSIM_STR_EXP(__A) #__A
+#define MVSIM_STR(__A)  MVSIM_STR_EXP(__A)
+#define MVSIM_VERSION   MVSIM_STR(MVSIM_MAJOR_VERSION) "." MVSIM_STR(MVSIM_MINOR_VERSION) "." MVSIM_STR(MVSIM_PATCH_VERSION)
+
+// clang-format on
