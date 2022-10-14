d1945893013cb03d09177e97d49e979ae67900b1
Jose Luis Blanco Claraco <joseluisblancoc@gmail.com>
2022-10-14 02:44:07 +0200

    Add Python3 example for teleop twist
    Former-commit-id: 6e1f9f334d3be18dd2970cf88a2037ee009b9158 [formerly b066aaf9b19897dde7dbddb7863ad298de479330]
    Former-commit-id: fc9b0fb952f99dc6b38f95750f4e70a3b3027b08

---
+++ mvsim_tutorial/python/mvsim-teleop.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+# ---------------------------------------------------------------------
+# This example shows how to:
+# - Call a MVSIM service.
+# - Use it to drive a vehicle/robot using its internal kinematics
+#   controller.
+#
+# Install python3-mvsim, or test with a local build with:
+# export PYTHONPATH=$HOME/code/mvsim/build/lib/:$PYTHONPATH
+# ---------------------------------------------------------------------
+
+from mvsim_comms import pymvsim_comms
+from mvsim_msgs import SrvSetControllerTwist_pb2
+import time
+import math
+
+
+def sendRobotTwistSetpoint(client, robotName, vx, vy, w):
+    # (vx,vy) in local coordinates [m/s]
+    # (w) in [rad/s]
+
+    req = SrvSetControllerTwist_pb2.SrvSetControllerTwist()
+    req.objectId = robotName  # vehicle/robot/object name in MVSIM
+    req.twistSetPoint.vx = vx
+    req.twistSetPoint.vy = vy
+    req.twistSetPoint.vz = 0
+    req.twistSetPoint.wx = 0
+    req.twistSetPoint.wy = 0
+    req.twistSetPoint.wz = w
+    # ret =
+    client.callService('set_controller_twist', req.SerializeToString())
+
+
+if __name__ == "__main__":
+    client = pymvsim_comms.mvsim.Client()
+    client.setName("mvsim-teleop")
+    print("Connecting to server...")
+    client.connect()
+    print("Connected successfully.")
+
+    sendRobotTwistSetpoint(client, 'r1', 1.0, 0.0, 0.0)
