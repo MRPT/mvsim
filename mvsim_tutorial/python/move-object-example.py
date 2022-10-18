12c5598f2437a0b6c4a7f95e7d4445a6062ee5f9
Jose Luis Blanco Claraco <joseluisblancoc@gmail.com>
2022-10-19 01:22:54 +0200

    Update instructions for python scripts with local builds

--- mvsim_tutorial/python/move-object-example.py
+++ mvsim_tutorial/python/move-object-example.py
@@ -6,7 +6,7 @@
 # - Use it to move an object in an arbitrary way.
 #
 # Install python3-mvsim, or test with a local build with:
-# export PYTHONPATH=$HOME/code/mvsim/build/lib/:$PYTHONPATH
+# export PYTHONPATH=$HOME/code/mvsim/build/:$PYTHONPATH
 # ---------------------------------------------------------------------
 
 from mvsim_comms import pymvsim_comms
