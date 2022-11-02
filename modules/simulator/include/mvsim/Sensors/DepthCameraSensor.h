5a7420e51a110297d4fda1b88b8dc4d4f99ed366
Jose Luis Blanco Claraco <joseluisblancoc@gmail.com>
2022-11-02 01:15:13 +0100

    FIX: more accurate simulation of timesteps; more mutexes
    Former-commit-id: df2dafd477569d3c75468db66d0968864dc708f1 [formerly 3013bce2e668a6c243b7a3e11494c12cf12ebb5e]
    Former-commit-id: 4d5e9c9a2170a132cb14c487c405e36472548427

--- modules/simulator/include/mvsim/Sensors/DepthCameraSensor.h
+++ modules/simulator/include/mvsim/Sensors/DepthCameraSensor.h
@@ -74,7 +74,9 @@ class DepthCameraSensor : public SensorBase
 	bool m_gui_uptodate = false;
 	std::recursive_mutex m_gui_mtx;
 	mrpt::opengl::CPointCloudColoured::Ptr m_gl_obs;
+
 	std::optional<TSimulContext> m_has_to_render;
+	std::mutex m_has_to_render_mtx;
 
 	float m_rgb_clip_min = 1e-2, m_rgb_clip_max = 1e+4;
 	float m_depth_clip_min = 0.1, m_depth_clip_max = 15.0;
