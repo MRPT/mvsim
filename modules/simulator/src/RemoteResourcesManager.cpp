d7d7971dfa50ab2c5799cde3276b831dc4d10255
Jose Luis Blanco Claraco <joseluisblancoc@gmail.com>
2023-01-08 17:27:15 +0100

    done with remote resources
    Former-commit-id: 81d19351c6bd26352c464cb97a61ef550f66f3a8 [formerly 0ef15af45bb41255068c2eaa6a4e48db9a7a5da7]
    Former-commit-id: 71164e74a16134485dc36819fd936e0a8139e655

--- modules/simulator/src/RemoteResourcesManager.cpp
+++ modules/simulator/src/RemoteResourcesManager.cpp
@@ -12,7 +12,6 @@
 
 #include <cstdlib>
 #include <cstring>
-#include <filesystem>
 
 using namespace mvsim;
 
@@ -45,18 +44,30 @@ std::string RemoteResourcesManager::resolve_path(const std::string& uri)
 
 std::string RemoteResourcesManager::cache_directory()
 {
-	std::filesystem::path local_directory;
+	std::string local_directory;
 #ifdef _WIN32
-	local_directory = std::filesystem::path(std::getenv("APPDATA"));
+	local_directory = std::getenv("APPDATA");
 #else
-	local_directory = std::filesystem::path(std::getenv("HOME"));
-	local_directory += std::filesystem::path("/.cache/");
+	local_directory = std::getenv("HOME");
+	local_directory += "/.cache/";
 #endif
 
-	local_directory += std::filesystem::path("mvsim/");
-	local_directory += std::filesystem::path("storage/");
+	local_directory += "mvsim-storage/";
 
-	return local_directory.string();
+	return local_directory;
+}
+
+std::tuple<bool, std::string, std::string>
+	RemoteResourcesManager::zip_uri_split(const std::string& uri)
+{
+	auto pos = uri.find(".zip/");
+
+	if (pos == std::string::npos) return {false, uri, ""};
+
+	const auto zipUri = uri.substr(0, pos + 4);
+	const auto internalUri = uri.substr(pos + 5);
+
+	return {true, zipUri, internalUri};
 }
 
 std::string RemoteResourcesManager::handle_remote_uri(const std::string& uri)
@@ -68,7 +79,15 @@ std::string RemoteResourcesManager::handle_remote_uri(const std::string& uri)
 
 	if (!mrpt::system::directoryExists(localDir))
 	{
-		std::filesystem::create_directories(localDir);
+#ifdef _WIN32
+		mrpt::system::createDirectory(localDir);
+#else
+		const auto cmd = "mkdir -p \""s + localDir + "\""s;
+		if (int ret = ::system(cmd.c_str()); ret != 0)
+		{
+			MRPT_LOG_ERROR_STREAM("Error executing command: " << cmd);
+		}
+#endif
 		ASSERT_DIRECTORY_EXISTS_(localDir);
 	}
 	else
@@ -85,39 +104,77 @@ std::string RemoteResourcesManager::handle_remote_uri(const std::string& uri)
 		"Using local storage directory: '"s + localDir + "' (Usage: "s +
 		cacheUsageStats + ")"s);
 
-	const auto fileName = mrpt::system::extractFileName(uri) + "." +
-						  mrpt::system::extractFileExtension(uri);
+	const auto [isZipPkg, zipOrFileURI, internalURI] = zip_uri_split(uri);
+
+	MRPT_LOG_DEBUG_STREAM(
+		"Split URI: isZipPkg=" << isZipPkg << " zipOrFileURI=" << zipOrFileURI
+							   << " internalURI=" << internalURI);
+
+	const auto fileName = mrpt::system::extractFileName(zipOrFileURI) + "." +
+						  mrpt::system::extractFileExtension(zipOrFileURI);
 	const auto localFil = localDir + fileName;
 
 	// Download if it does not exist already from a past download:
 	if (!mrpt::system::fileExists(localFil))
 	{
-		const auto cmd =
-			mrpt::format("wget -q -O \"%s\" %s", localFil.c_str(), uri.c_str());
+		const auto cmd = mrpt::format(
+			"wget -q -O \"%s\" %s", localFil.c_str(), zipOrFileURI.c_str());
 
 		MRPT_LOG_INFO_STREAM(
 			"Downloading remote resources from: '" << uri << "'");
 
-		int ret = ::system(cmd.c_str());
-		if (ret != 0)
+		if (int ret = ::system(cmd.c_str()); ret != 0)
 		{
 			THROW_EXCEPTION_FMT(
-				"[mvsim] Error executing the following command trying to "
-				"acquire a remote resource:\n%s",
-				cmd.c_str());
+				"[mvsim] Error (code=%i)  executing the following command "
+				"trying to acquire a remote resource:\n%s",
+				ret, cmd.c_str());
 		}
 	}
 
 	// Is it a simple model file, or a zip?
-	if (0)
+	if (isZipPkg)
 	{
 		// process the ZIP package:
-
-		return {};
+		return handle_local_zip_package(localFil, internalURI);
 	}
 	else
 	{
 		// simple file:
 		return localFil;
 	}
+}
+
+std::string RemoteResourcesManager::handle_local_zip_package(
+	const std::string& localZipFil, const std::string& internalURI)
+{
+	using namespace std::string_literals;
+
+	ASSERT_FILE_EXISTS_(localZipFil);
+
+	const auto filExtension = mrpt::system::extractFileExtension(localZipFil);
+	ASSERT_EQUAL_(filExtension, "zip");
+
+	// already decompressed?
+	const auto zipOutDir =
+		mrpt::system::fileNameChangeExtension(localZipFil, "out");
+
+	if (!mrpt::system::directoryExists(zipOutDir))
+	{
+		mrpt::system::createDirectory(zipOutDir);
+		ASSERT_DIRECTORY_EXISTS_(zipOutDir);
+
+		const std::string cmd =
+			"unzip \""s + localZipFil + "\" -q -d \""s + zipOutDir + "\""s;
+
+		if (int ret = ::system(cmd.c_str()); ret != 0)
+		{
+			THROW_EXCEPTION_FMT(
+				"[mvsim] Error (code=%i) executing the following command "
+				"trying to unzip a remote resource:\n%s",
+				ret, cmd.c_str());
+		}
+	}
+
+	return zipOutDir + "/"s + internalURI;
 }
