30774b9339a00f0654a5b1024610d7c1a8ae5d56
Jose Luis Blanco Claraco <joseluisblancoc@gmail.com>
2023-01-24 01:00:53 +0100

    progress with N agents demo

--- mvsim_tutorial/python/simple-obstacle-avoidance.py
+++ mvsim_tutorial/python/simple-obstacle-avoidance.py
@@ -9,15 +9,22 @@
 #
 # Install python3-mvsim, or test with a local build with:
 # export PYTHONPATH=$HOME/code/mvsim/build/:$PYTHONPATH
+#
+# Demo with N robots:
+# for i in $(seq 1 50); do bash -c "mvsim_tutorial/python/simple-obstacle-avoidance.py --vehicle veh${i} &"; done
+#
 # ---------------------------------------------------------------------
 
 import argparse
 import time
+import math
 from mvsim_comms import pymvsim_comms
 from mvsim_msgs import SrvSetControllerTwist_pb2
 from mvsim_msgs import ObservationLidar2D_pb2
 
-OBS_AVOIDANCE_PERIOD = 0.2
+OBS_AVOIDANCE_PERIOD = 0.1
+V_MAX = 1.0
+W_MAX = 1.0
 
 parser = argparse.ArgumentParser(prog='simple-obstacle-avoidance')
 parser.add_argument('--vehicle', dest='vehicleName', action='store', required=True,
@@ -51,13 +58,42 @@ def evalObstacleAvoidance(obs: ObservationLidar2D_pb2.ObservationLidar2D):
     ang = -obs.aperture*0.5
     dA = obs.aperture / (len(obs.scanRanges)-1)
 
+    # Force vectors
+    resultantForce = [0, 0]
+
+    #obsWeight = 20.0 / len(obs.scanRanges)
+    obsWeight = 1
+
     for idx, r in enumerate(obs.scanRanges):
-        print(idx, r, ang)
         ang += dA
-    # obs.maxRange
-    # obs.scanRanges
-    # obs.sensorPose
-    #
+        # obs.maxRange
+        # obs.scanRanges
+        # obs.sensorPose
+        #
+        # Obstacles:
+        # Compute force strength:
+        mod = min(1e3, 1.0 / r)
+
+        # Add repulsive force:
+        fx = -math.cos(ang) * mod
+        fy = -math.sin(ang) * mod
+        resultantForce[0] += fx * obsWeight
+        resultantForce[1] += fy * obsWeight
+
+    # Target:
+    # mod = options.TARGET_ATTRACTIVE_FORCE
+    # resultantForce += [cos(ang) * mod, sin(ang) * mod]
+
+    # Result:
+    desiredDirection = 0
+    if (resultantForce[0] != 0) or (resultantForce[1] != 0):
+        desiredDirection = math.atan2(resultantForce[1], resultantForce[0])
+
+    # Convert direction to differential-driven command:
+    v = V_MAX*math.cos(desiredDirection)
+    w = W_MAX*desiredDirection/math.pi * math.copysign(1.0, v)
+
+    return [v, w]
 
 
 def onLidar2DMessage(msgType, msg):
@@ -66,11 +102,12 @@ def onLidar2DMessage(msgType, msg):
     assert(msgType == "mvsim_msgs.ObservationLidar2D")
     p = ObservationLidar2D_pb2.ObservationLidar2D()
     p.ParseFromString(bytes(msg))
-    #print("[lidar callback] received:\n ranges=\n" + str(p.scanRanges) + "\n validRanges=" + str(p.validRanges))
+    # print("[lidar callback] received:\n ranges=\n" + str(p.scanRanges) + "\n validRanges=" + str(p.validRanges))
 
     if (p.unixTimestamp > prevLidarMsgTimestamp+OBS_AVOIDANCE_PERIOD):
         prevLidarMsgTimestamp = p.unixTimestamp
-        evalObstacleAvoidance(p)
+        [v, w] = evalObstacleAvoidance(p)
+        sendRobotTwistSetpoint(client, args.vehicleName, v, 0, w)
 
 
 if __name__ == "__main__":
@@ -88,6 +125,5 @@ if __name__ == "__main__":
     client.subscribeTopic("/" + args.vehicleName +
                           "/laser1_scan", onLidar2DMessage)
 
-    sendRobotTwistSetpoint(client, args.vehicleName, 0.75, 0.0, 1.0)
-
-    time.sleep(10)
+    time.sleep(20)
+    sendRobotTwistSetpoint(client, args.vehicleName, 0, 0, 0)
