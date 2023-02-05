087ba027f7f7f273c27d1e8ef44cf9b45dd58607
Jose Luis Blanco Claraco <joseluisblancoc@gmail.com>
2023-02-05 11:12:51 +0100

    FIX: <for> loops ignored more than one inner tag

--- modules/simulator/src/World_load_xml.cpp
+++ modules/simulator/src/World_load_xml.cpp
@@ -242,8 +242,12 @@ void World::internal_recursive_parse_XML(
 			varTo, "XML tag '<for />' must have a 'to=\"xxx\"' attribute)");
 		const auto toStr = mvsim::parse(varTo->value(), userDefinedVariables_);
 
-		if (auto childNode = node->first_node(); childNode)
+		bool forBodyEmpty = true;
+
+		for (auto childNode = node->first_node(); childNode;
+			 childNode = childNode->next_sibling())
 		{
+			forBodyEmpty = false;
 			for (int curVal = std::stoi(fromStr); curVal <= std::stoi(toStr);
 				 curVal++)
 			{
@@ -254,7 +258,8 @@ void World::internal_recursive_parse_XML(
 				internal_recursive_parse_XML(childNode, currentBasePath);
 			}
 		}
-		else
+
+		if (forBodyEmpty)
 		{
 			MRPT_LOG_WARN_STREAM(
 				"[World::load_from_XML] *Warning* <for ...> </for> loop has no "
