72f1f6a1f63755d2345830d4f312c4be178fd70c
Jose Luis Blanco Claraco <joseluisblancoc@gmail.com>
2023-03-24 11:18:26 +0100

    more automated testing

--- tests/test_shape2p5.cpp
+++ tests/test_shape2p5.cpp
@@ -7,14 +7,20 @@
   |   See COPYING                                                           |
   +-------------------------------------------------------------------------+ */
 
+#include <mrpt/opengl/CCylinder.h>
 #include <mrpt/opengl/CSphere.h>
+#include <mrpt/system/os.h>
 #include <mvsim/CollisionShapeCache.h>
 
+#include <functional>
 #include <iostream>
+#include <vector>
+
+#include "test_utils.h"
 
 using namespace mvsim;
 
-void test_shape1()
+void shape_test_sphere()
 {
 	auto& csc = CollisionShapeCache::Instance();
 	const float radius = 0.5f;
@@ -25,16 +31,50 @@ void test_shape1()
 	std::cout << shape.getContour() << std::endl;
 }
 
+void shape_test_cylinder()
+{
+	auto& csc = CollisionShapeCache::Instance();
+
+	const float radius = 0.5f, L = 2.0f;
+	auto glCyl = mrpt::opengl::CCylinder::Create(radius, radius, L);
+
+	const Shape2p5 shape = csc.get(*glCyl, 0, L, {}, 1.0f);
+
+	std::cout << shape.getContour() << std::endl;
+}
+
 int main([[maybe_unused]] int argc, [[maybe_unused]] char** argv)
 {
-	try
+	std::vector<std::pair<std::function<void(void)>, std::string>> lst = {
+		{&shape_test_sphere, "shape_test_sphere"},
+		{&shape_test_cylinder, "shape_test_cylinder"},
+	};
+
+	bool anyFail = false;
+
+	for (const auto& kv : lst)
 	{
-		test_shape1();
-		return 0;
+		try
+		{
+			setConsoleBlueColor();
+			printf("[%20s] Running...\n", kv.second.c_str());
+			setConsoleNormalColor();
+
+			kv.first();
+
+			setConsoleBlueColor();
+			printf("[%20s] Success\n", kv.second.c_str());
+			setConsoleNormalColor();
+		}
+		catch (const std::exception& e)
+		{
+			std::cerr << e.what();
+			setConsoleErrorColor();
+			printf("[%20s] FAIL\n", kv.second.c_str());
+			setConsoleNormalColor();
+			anyFail = true;
+		}
 	}
-	catch (const std::exception& e)
-	{
-		std::cerr << e.what();
-		return 1;
-	}
+
+	return anyFail ? 1 : 0;
 }
