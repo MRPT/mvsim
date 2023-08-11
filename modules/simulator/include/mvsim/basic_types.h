e1063c2c91c04f11feb6c64247c6804bb4ba54cc
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2023-08-11 17:30:41 +0200

    fix format

--- modules/simulator/include/mvsim/basic_types.h
+++ modules/simulator/include/mvsim/basic_types.h
@@ -14,7 +14,7 @@
 #include <mrpt/math/TPose2D.h>
 #include <mrpt/math/TTwist2D.h>
 
-#include <cstdint> // uintptr_t
+#include <cstdint>	// uintptr_t
 #include <string>
 #include <vector>
 
@@ -22,43 +22,54 @@ class b2World;
 class b2Body;
 class b2Fixture;
 
-namespace rapidxml {
+namespace rapidxml
+{
 // Forward declarations
-template <class Ch> class xml_node;
-template <class Ch> class xml_attribute;
-template <class Ch> class xml_document;
-} // namespace rapidxml
+template <class Ch>
+class xml_node;
+template <class Ch>
+class xml_attribute;
+template <class Ch>
+class xml_document;
+}  // namespace rapidxml
 
-namespace mrpt {
-namespace opengl {
+namespace mrpt
+{
+namespace opengl
+{
 class CSetOfObjects;
-} // namespace opengl
-namespace slam {
+}  // namespace opengl
+namespace slam
+{
 class CObservation;
 }
-} // namespace mrpt
+}  // namespace mrpt
 
-namespace mvsim {
+namespace mvsim
+{
 class World;
 class VehicleBase;
-template <typename Ch = char> class JointXMLnode;
+template <typename Ch = char>
+class JointXMLnode;
 
 /** Simulation context for simulable objects updates */
-struct TSimulContext {
-  b2World *b2_world = nullptr;
-  World *world = nullptr;
-  double simul_time = 0; //!< Current time in the simulated world
-  double dt = 0;         //!< timestep
+struct TSimulContext
+{
+	b2World* b2_world = nullptr;
+	World* world = nullptr;
+	double simul_time = 0;	//!< Current time in the simulated world
+	double dt = 0;	//!< timestep
 };
 
 /// Used to signal a Box2D fixture as "invisible" to sensors.
 constexpr uintptr_t INVISIBLE_FIXTURE_USER_DATA = 1;
 
-struct TJoyStickEvent {
-  float x = .0f, y = .0f, z = .0f;
-  std::vector<bool> buttons;
+struct TJoyStickEvent
+{
+	float x = .0f, y = .0f, z = .0f;
+	std::vector<bool> buttons;
 
-  TJoyStickEvent() = default;
+	TJoyStickEvent() = default;
 };
 
-} // namespace mvsim
+}  // namespace mvsim
