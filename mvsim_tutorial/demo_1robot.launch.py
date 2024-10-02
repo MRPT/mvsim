dd2c8d59a488e1ca05e2a4338629f9270837ce6c
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2024-10-03 00:39:32 +0200

    demo_1robot: add launch arguments headless and use_rviz

--- mvsim_tutorial/demo_1robot.launch.py
+++ mvsim_tutorial/demo_1robot.launch.py
@@ -5,6 +5,7 @@ from launch import LaunchDescription
 from launch.substitutions import TextSubstitution
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
+from launch.conditions import IfCondition
 from launch.actions import DeclareLaunchArgument
 from ament_index_python import get_package_share_directory
 import os
@@ -20,6 +21,12 @@ def generate_launch_description():
             text=os.path.join(mvsimDir, 'mvsim_tutorial', 'demo_1robot.world.xml')))
     do_fake_localization_arg = DeclareLaunchArgument(
         "do_fake_localization", default_value='True', description='publish tf odom -> base_link')
+    headless_launch_arg = DeclareLaunchArgument(
+        "headless", default_value='False')
+    use_rviz_arg = DeclareLaunchArgument(
+        'use_rviz', default_value='True',
+        description='Whether to launch RViz2'
+    )
 
     mvsim_node = Node(
         package='mvsim',
@@ -30,6 +37,7 @@ def generate_launch_description():
             os.path.join(mvsimDir, 'mvsim_tutorial', 'mvsim_ros2_params.yaml'),
             {
                 "world_file": LaunchConfiguration('world_file'),
+                "headless": LaunchConfiguration('headless'),
                 "do_fake_localization": LaunchConfiguration('do_fake_localization'),
             }]
     )
@@ -39,12 +47,15 @@ def generate_launch_description():
         executable='rviz2',
         name='rviz2',
         arguments=[
-                '-d', [os.path.join(mvsimDir, 'mvsim_tutorial', 'demo_1robot_ros2.rviz')]]
+                '-d', [os.path.join(mvsimDir, 'mvsim_tutorial', 'demo_1robot_ros2.rviz')]],
+        condition=IfCondition(LaunchConfiguration('use_rviz'))
     )
 
     return LaunchDescription([
         world_file_launch_arg,
         do_fake_localization_arg,
         mvsim_node,
-        rviz2_node
+        use_rviz_arg,
+        rviz2_node,
+        headless_launch_arg,
     ])
