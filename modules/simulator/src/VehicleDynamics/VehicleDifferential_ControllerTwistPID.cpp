6fbe4882618df7e3d40f6e790913986e3b5cc421
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2025-02-04 12:22:45 +0100

    BUGFIX: Crash due to access uninitialized memory when no joystick is found

--- modules/simulator/src/VehicleDynamics/VehicleDifferential_ControllerTwistPID.cpp
+++ modules/simulator/src/VehicleDynamics/VehicleDifferential_ControllerTwistPID.cpp
@@ -127,7 +127,7 @@ void DynamicsDifferential::ControllerTwistPID::teleop_interface(
 
 	out.append_gui_lines += "[Controller=" + std::string(class_name()) + "]";
 
-	if (in.js)
+	if (in.js && in.js->axes.size() >= 2)
 	{
 		const auto& js = in.js.value();
 		const float js_x = js.axes[0];
