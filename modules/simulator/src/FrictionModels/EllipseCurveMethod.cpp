3f42b2e980c543a19f2718fcc42d066180c82062
fpi684 <fpi684@inlumine.ual.es>
2025-03-31 17:33:16 +0200

    pr35

--- modules/simulator/src/FrictionModels/EllipseCurveMethod.cpp
+++ modules/simulator/src/FrictionModels/EllipseCurveMethod.cpp
@@ -88,9 +88,9 @@ mrpt::math::TVector2D EllipseCurveMethod::evaluate_friction(
 
 	//-------------------------------------------------------------------------
 	// Valores que no sé si estoy tomando correctamente
-	const mrpt::math::TPoint3D_<double> linAccGlobal = myVehicle_.getLinearAcceleration();
+	const mrpt::math::TPoint2D_<double> linAccGlobal = myVehicle_.getLinearAcceleration();
 	const mrpt::poses::CPose2D vehiclePose = myVehicle_.getPose();
-	mrpt::math::TPoint3D_<double> linAccLocal;
+	mrpt::math::TPoint2D_<double> linAccLocal;
 	vehiclePose.inverseComposePoint(linAccGlobal, linAccLocal);
 	// const mrpt::math::TVector2D linAccLocal = getAcc();
 	//  ¿Está bien? no se si se corresponde con la aceleración que quiero
