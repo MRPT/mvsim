4b6728762d44ff6cde6096ad78ce621172d58367
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2025-06-27 01:05:53 +0200

    Update copyright notes

--- modules/simulator/src/FrictionModels/EllipseCurveMethod.cpp
+++ modules/simulator/src/FrictionModels/EllipseCurveMethod.cpp
@@ -1,7 +1,8 @@
 /*+-------------------------------------------------------------------------+
   |                       MultiVehicle simulator (libmvsim)                 |
   |                                                                         |
-  | Copyright (C) 2014-2023  Jose Luis Blanco Claraco                       |
+  | Copyright (C) 2025 Francisco Pérez Ibañez                               |
+  | Copyright (C) 2014-2025  Jose Luis Blanco Claraco                       |
   | Copyright (C) 2017  Borys Tymchenko (Odessa Polytechnic University)     |
   | Distributed under 3-clause BSD License                                  |
   |   See COPYING                                                           |
