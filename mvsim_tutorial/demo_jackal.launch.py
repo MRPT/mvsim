8615ccb07372f1a9744fe4f4b630c4f8cdd65a66
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2026-01-23 23:43:13 +0100

    fix launch formatting and inconsistent defaults

--- mvsim_tutorial/demo_jackal.launch.py
+++ mvsim_tutorial/demo_jackal.launch.py
@@ -1,47 +1,60 @@
+# ROS2 launch file for Jackal robot demo world
 
-# ROS2 launch file
-
-from launch import LaunchDescription
-from launch.substitutions import TextSubstitution
-from launch.substitutions import LaunchConfiguration
-from launch_ros.actions import Node
-from launch.actions import DeclareLaunchArgument
-from ament_index_python import get_package_share_directory
 import os
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import TextSubstitution, LaunchConfiguration
+from launch_ros.actions import Node
+from ament_index_python import get_package_share_directory
 
 
 def generate_launch_description():
-    mvsimDir = get_package_share_directory("mvsim")
-    # print('mvsimDir: ' + mvsimDir)
+    # Get mvsim package directory
+    mvsim_dir = get_package_share_directory('mvsim')
 
-    # args that can be set from the command line or a default will be used
+    # Launch configuration variables (no defaults here)
+    world_file = LaunchConfiguration('world_file')
+    do_fake_localization = LaunchConfiguration('do_fake_localization')
+
+    # Declare launch arguments (defaults defined here)
     world_file_launch_arg = DeclareLaunchArgument(
-        "world_file", default_value=TextSubstitution(
-            text=os.path.join(mvsimDir, 'mvsim_tutorial', 'demo_jackal.world.xml')))
+        'world_file',
+        default_value=TextSubstitution(
+            text=os.path.join(mvsim_dir, 'mvsim_tutorial', 'demo_jackal.world.xml')
+        ),
+        description='Path to world XML file'
+    )
     do_fake_localization_arg = DeclareLaunchArgument(
-        "do_fake_localization", default_value='True', description='publish tf odom -> base_link')
+        'do_fake_localization',
+        default_value='True',
+        description='Publish tf odom -> base_link'
+    )
 
+    # MVSim node
     mvsim_node = Node(
         package='mvsim',
         executable='mvsim_node',
         name='mvsim',
         output='screen',
         parameters=[
-            os.path.join(mvsimDir, 'mvsim_tutorial',
-                         'mvsim_ros2_params.yaml'),
+            os.path.join(mvsim_dir, 'mvsim_tutorial', 'mvsim_ros2_params.yaml'),
             {
-                "world_file": LaunchConfiguration('world_file'),
-                "headless": False,
-                "do_fake_localization": LaunchConfiguration('do_fake_localization'),
-            }]
+                'world_file': world_file,
+                'headless': False,
+                'do_fake_localization': do_fake_localization,
+            }
+        ]
     )
 
+    # RViz2 node
     rviz2_node = Node(
         package='rviz2',
         executable='rviz2',
         name='rviz2',
         arguments=[
-                '-d', [os.path.join(mvsimDir, 'mvsim_tutorial', 'demo_depth_camera_ros2.rviz')]]
+            '-d',
+            os.path.join(mvsim_dir, 'mvsim_tutorial', 'demo_depth_camera_ros2.rviz')
+        ]
     )
 
     return LaunchDescription([
